86049c6459752a9828acab9ee73584a61f857811
Luis Hernandez <lhdez@iuma.ulpgc.es>
2019-03-20 13:46:22 +0000

    cambios menores

--- app/src/main/java/es/ulpgc/eite/cleancode/livedata/counter/CounterRouter.java
+++ app/src/main/java/es/ulpgc/eite/cleancode/livedata/counter/CounterRouter.java
@@ -1,6 +1,5 @@
 package es.ulpgc.eite.cleancode.livedata.counter;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 
@@ -14,19 +13,27 @@ public class CounterRouter implements CounterContract.Router {
 
   public static String TAG = CounterRouter.class.getSimpleName();
 
-  //private WeakReference<FragmentActivity> context;
+  private WeakReference<FragmentActivity> context;
   private AppMediator mediator;
 
   public CounterRouter(WeakReference<FragmentActivity> context) {
-    //this.context = context;
+    this.context = context;
     mediator = (AppMediator) context.get().getApplication();
   }
 
+  /*
   @Override
   public void navigateToNextScreen() {
     Context context = mediator.getApplicationContext();
     Intent intent = new Intent(context, ResetActivity.class);
     context.startActivity(intent);
+  }
+  */
+
+  @Override
+  public void navigateToNextScreen() {
+    Intent intent = new Intent(context.get(), ResetActivity.class);
+    context.get().startActivity(intent);
   }
 
   @Override
